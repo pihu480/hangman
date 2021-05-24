e3544eb78acff7bd219b7b187746b3acb1dc546c
pihu480 <84699440+pihu480@users.noreply.github.com>
2021-05-24 11:51:55 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,19 @@
+import string
+import random
+
+
+def load_words():
+
+    inFile = open('words.text', 'r')
+    line = inFile.readline()
+    word_list = line.split()
+
+    return word_list 
+
+def choose_word():
+    
+    word_list = load_words()
+    secret_word = random.choice(word_list)
+    secret_word = secret_word.lower()
+
+    return secret_word
